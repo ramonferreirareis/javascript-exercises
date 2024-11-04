f4ad29f79e7a403684bcac64d2fbffe0e507c5c6
Ramon Reis <111209853+ramonferreirareis@users.noreply.github.com>
2024-11-04 15:48:48 -0300

    Complete helloWorld, repeatString and reverseString exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num === 0) {
+        return "";
+    } else if (num < 0) {
+        return "ERROR";
+    } else {
+        let repeatedString = string;
+        for (i = 1; i < num; i++) {
+            repeatedString += string;
+        }
+        return repeatedString;
+    }
 };
 
 // Do not edit below this line
