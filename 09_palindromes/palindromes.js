e15d1a4ba854adcaab57dc405480f6660e792574
Ramon Reis <111209853+ramonferreirareis@users.noreply.github.com>
2024-11-23 19:14:40 -0300

    Add function that checks if string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const rawString = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    const reversedString = string.split("").reverse().join("").toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+
+    if (rawString === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
