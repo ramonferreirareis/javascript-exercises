404e640fd6abfce76fb54461d099fe6b96c6f0b0
Ramon Reis <111209853+ramonferreirareis@users.noreply.github.com>
2024-11-23 18:44:07 -0300

    Add multiple functions of a calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  } else {
+    return arr.reduce((total, value) => total + value);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, value) => total * value);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0 || number === 1) {
+    return 1;
+  } else {
+    let sum = number;
+    let fac = number - 1;
+    for (let i = 0; i < number; i++) {
+      sum = sum * fac;
+      if(fac > 1) fac--;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
