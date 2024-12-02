45760a13de6f301d0b1a3f3b61f9d761a4fda3dd
Ramon Reis <111209853+ramonferreirareis@users.noreply.github.com>
2024-12-01 21:33:07 -0300

    Add function to give the fibonacci number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+   let fib1 = 1;
+   let fib2 = 1;
+   let sum = 0;
+   
+   if (typeof num != "number") {
+    num = Number(num);
+   }
 
+   if (num < 0) return "OOPS";
+   if (num === 0) return 0;
+
+   if (num <= 2) {
+    return 1;
+   } else {
+    num -= 2;
+    for(let i = 0; i < num; i++) {
+        sum = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = sum;
+       }
+   }
+
+   return sum;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
